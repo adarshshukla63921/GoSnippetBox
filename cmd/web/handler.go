2d9da85ff3f60f784d58650a4a9562a9d5463ec3
adarshshukla63921 <adarshshukla63921@gmail.com>
2026-01-09 22:05:35 +0530

    Html templating and added resources in ui/static

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -13,18 +12,23 @@ func home(w http.ResponseWriter, r *http.Request){
 		http.NotFound(w,r)
 		return
 	}
-	ts , err := template.ParseFiles("ui\\html\\home.html")
+	files := []string{
+		"ui\\html\\base.html",
+		"ui\\html\\nav.html",
+		"ui\\html\\pages\\home.html",
+	}
 
-	if(err!=nil){
-		log.Print(err.Error())
-		http.Error(w,"something went wrong",http.StatusInternalServerError)
+	ts , err := template.ParseFiles(files...)
+
+	if err != nil {
+		http.Error(w,"Something went wrong",http.StatusInternalServerError)
 		return
 	}
 
-	err = ts.Execute(w,nil)
+	err = ts.ExecuteTemplate(w,"base",nil)
+
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w,"something went wrong rendering the page",http.StatusInternalServerError)
+		http.Error(w,"Something went wrong",http.StatusInternalServerError)
 		return
 	}
 }
--- structure.md
+++ structure.md
@@ -1,4 +1,4 @@
-Using this structure gives 2 main advantages.
+# Using this structure gives 2 main advantages
 
 1) Clear seperation of Go and non-Go assets. Go code will live in cmd and internal folders. Easy to manage and deploy app in future.
 
